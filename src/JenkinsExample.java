d70ec5dea2647aa23dd3cf762917698c6ccf03ef
Sailaja Polusetti <spoluset@in.ibm.com>
2019-12-03 15:22:51 +0530

    Comments changed

--- src/JenkinsExample.java
+++ src/JenkinsExample.java
@@ -1,4 +1,4 @@
-//HelloWorld Program
+//Using below program to build and test project using Jenkins.
 public class JenkinsExample {
 	public static void main(String args[]){
 		System.out.println("First Java program building by Jenkins");
